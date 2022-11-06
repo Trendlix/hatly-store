83204c35193017c135c344132eff49e7cf9c5788
Hamed Osama <hamedosama19@gmail.com>
2022-11-06 17:18:39 +0200

    update sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import CloseWrapper from "../CloseBtn/CloseWrapper";
 import { useSelector } from "react-redux";
 import { isSidebarOpened } from "../../features/sidebar/sidebar-slice";
+import { Link } from "react-router-dom";
 // import CloseWrapper from '../UI/CloseWrapper'
 // import style from './Sidebar.module.css'
 
@@ -9,26 +10,26 @@ const SideBar: FC = (props) => {
   const isOpened = useSelector(isSidebarOpened);
   return (
     <div
-      className={`fixed top-0 bottom-0 right-0 bg-secondary ml-1 z-[1000] w-[300px] duration-150 translate-x-[100%] ${
+      className={`fixed top-0 bottom-0 right-0 sidebar_bg ml-1 z-[1000] w-[300px] duration-150 translate-x-[100%] ${
         isOpened === true ? "translate-x-0" : ""
       }`}
     >
       <CloseWrapper />
       <ul className="flex flex-col text-start py-4 px-8">
         <li className="sidebar__element">
-          <a href="#tesla">Home</a>
+          <Link to="/">Home</Link>
         </li>
         <li className="sidebar__element">
-          <a href="#tesla">About us</a>
+          <Link to="#tesla">About us</Link>
         </li>
         <li className="sidebar__element">
-          <a href="#tesla">Our Services</a>
+          <Link to="#tesla">Our Services</Link>
         </li>
         <li className="sidebar__element">
-          <a href="#tesla">Events</a>
+          <Link to="#tesla">Events</Link>
         </li>
         <li className="sidebar__element">
-          <a href="#tesla">Contact Us</a>
+          <Link to="/contact-us">Contact Us</Link>
         </li>
       </ul>
     </div>
