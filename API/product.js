420e9d0cf75a7bf84cd7a2d86a89a326fbfa660f
Hamed Osama <hamedosama19@gmail.com>
2022-12-11 18:15:18 +0200

    test deploy

--- API/product.js
+++ API/product.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 // const productURL = "https://hatlyback.herokuapp.com/api/";
+// const productURL = "http://localhost:8000/api/";
 const productURL = "https://hatly-server.onrender.com/api/";
 
 export const fetchProduct = axios.create({
