b51c5b7bed2c714481fa30249e87156882e9a551
Hamed Osama <hamedosama19@gmail.com>
2022-12-15 19:37:01 +0200

    adjust image size in home banners

--- componants/pages/home/CategoryDevider.jsx
+++ componants/pages/home/CategoryDevider.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Image from "next/image";
 
 const CategoryDevider = (props) => {
+  console.log(props)
   return (
     <Link onClick={() => {
       window.scroll({
@@ -12,7 +13,7 @@ const CategoryDevider = (props) => {
       });
     }} href={`/shop/${props.url}`}>
       <section className="container mb-4">
-        <Image src={props.img} alt="" className="img" loading="lazy" />
+        <Image src={props.img} alt="banner" width={props.img.width} height={props.img.width} blurDataURL={props.img.blurDataURL} className="img" />
         {/* <h1 className="text-center" style={{ color: "white" }}>
             {props.name}
           </h1> */}
