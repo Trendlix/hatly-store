f34928fd9530b4ba46d630f5c52ca1dd14bc2a6c
Hamed Osama <hamedosama19@gmail.com>
2023-01-01 20:53:09 +0200

    fix syntax error

--- utils/makeOrder.js
+++ utils/makeOrder.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import API_URL from "../API/ApiUrl"
 
-axios.defaults.withCredentials = True;
+axios.defaults.withCredentials = true;
 
 const makeOrder = async (data,cart,paymentMethod) => {
   try {
