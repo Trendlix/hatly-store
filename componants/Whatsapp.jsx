a5e1a0d8a777d2ac8be4171b9e51aa304d59cf55
Hamed Osama <hamedosama19@gmail.com>
2023-01-25 16:33:54 +0200

    add aria-label for whatapp icon

--- componants/Whatsapp.jsx
+++ componants/Whatsapp.jsx
@@ -6,7 +6,7 @@ import {
 const Whatsapp = () => {
     return (
         <div style={{ position: 'fixed', zIndex: '9999', bottom: '0px', right: "0" }}>
-            <a href="https://api.whatsapp.com/send?phone=201558807773" target="_blank" rel="noreferrer">
+            <a href="https://api.whatsapp.com/send?phone=201558807773" aria-label='WhatsApp' target="_blank" rel="noreferrer">
                 <div className='d-flex align-items-center justify-content-center' style={{ width: '65px', height: '65px', borderRadius: '50%', backgroundColor: '#3a4c8d', margin: "30px" }}>
                     <FontAwesomeIcon icon={faWhatsapp} style={{ color: 'white', fontSize: '40px' }}></FontAwesomeIcon>
                 </div>
