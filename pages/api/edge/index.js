75b0129e0bd031c7165a3b4dc676d1e05874accb
Hamed Osama <hamedosama19@gmail.com>
2023-04-09 03:56:57 +0200

    try with cache

--- pages/api/edge/index.js
+++ pages/api/edge/index.js
@@ -5,8 +5,25 @@ export const config = {
   // regions: ['iad1'], // only execute this function on iad1
 };
 
-export default function handler(req, res) {
-  return NextResponse.json({
-    name: `Hello, from ${req.url} I'm now an Edge Function!`,
-  });
+export default async function handler(req) {
+  return new Response(
+    JSON.stringify({
+      name: 'John Doe',
+    }),
+    {
+      status: 200,
+      headers: {
+        'Cache-Control': 's-maxage=86400',
+        'Content-Type': 'application/json',
+      },
+    },
+  );
 }
+// export default function handler(req, res) {
+//   return NextResponse.headers({
+//     'Content-Type': 'application/json',
+//     'Cache-Control': 's-maxage=1, stale-while-revalidate',
+//   }).json({
+//     name: `Hello, from ${req.url} I'm now an Edge Function!`,
+//   });
+// }
