3960daf1420729d35d49a7377ece170204c20837
marianWadia <marianwadia55@gmail.com>
2024-06-25 14:28:10 +0300

    space remove to trigger vercel

--- API/product.js
+++ API/product.js
@@ -5,7 +5,6 @@ const productURL = "https://hatly-backend.onrender.com/api/v1";
 // const productURL = "https://hatly-server.onrender.com/api/";
 // const productURL = "https://hatlyapi.trendlix.com/api/"
 
-
 export const fetchProduct = axios.create({
   baseURL: productURL,
 });
